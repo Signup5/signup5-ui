0e87a958cb070c6032c660ed099c0d931e059dc7
Kristian Grundström <Kristian.Grundstrom@yh.nackademin.se>
2020-03-12 12:19:55 +0100

    Render My Events in dashboard

---
+++ src/Components/Dashboard/RenderMyEvents.tsx
@@ -0,0 +1,49 @@
+import { useQuery } from "@apollo/react-hooks";
+import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import { GET_EVENTS_BY_HOST_ID } from "../../Store/GQL";
+import { InitialState } from "../../Store/Reducers/rootReducer";
+import { Event, Person, QueryResponse } from "../../Types";
+import { RenderEvent } from "./RenderEvent";
+
+interface Props {}
+
+interface StateProps {
+  person: Person;
+}
+
+export const RenderMyEvents: FC<Props> = () => {
+  const stateProps = useSelector<InitialState, StateProps>(
+    (state: InitialState) => {
+      return {
+        person: state.person
+      };
+    }
+  );
+  const response: QueryResponse = useQuery(GET_EVENTS_BY_HOST_ID, {
+    variables: {
+      id: stateProps.person.id
+    }
+  });
+
+  if (response.loading) return <p>Loading...</p>;
+  if (response.error) {
+    return <p>ERROR</p>;
+  }
+
+  const events: Array<Event> = response.data.events;
+
+  console.log(stateProps.person);
+
+  const render = () => {
+    return events.map((event, index) => {
+      return <RenderEvent key={index} event={event} />;
+    });
+  };
+
+  return (
+    <div>
+      <ul>{render()}</ul>
+    </div>
+  );
+};
