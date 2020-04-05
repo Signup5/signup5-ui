ce7e06ccb07974ef2852c06c01465fb6c30ae8e2
David Södergren <david.sodergren@pricer.com>
2020-04-05 11:25:22 +0200

    added logout button

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,38 @@ import PasswordLink from "./Components/PasswordLink";
 import { CreateEventForm } from "./Components/Dashboard/CreateEvent";
 import { Dashboard } from "./Components/Dashboard";
 import { ProtectedRoute } from "./Components/ProtectedRoute";
+import { Button, makeStyles } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    background: "#f08700",
+    position: "absolute",
+    right: "30px"
+  }
+});
 
 const App: FC = () => {
+  const history = useHistory();
+  const classes = useStyles();
+  const logout = () => {
+    localStorage.setItem("person", "");
+    localStorage.setItem("token", "");
+    history.push("/");
+  };
   return (
     <div className={Classes.App}>
       <div className={Classes.AppHeader}>
         <SignupLogo />
+        <Button
+          size="large"
+          classes={{
+            root: classes.root // class name, e.g. `classes-nesting-root-x`
+          }}
+          onClick={() => logout()}
+        >
+          Logout
+        </Button>
       </div>
       <div className={Classes.AppMainContent}>
         <Switch>
