a34ce9e0713800bcb1a0a6e5db80b7e9dc276741
Kristian Grundström <kristian.grundstrom@yh.nackademin.se>
2020-04-10 15:35:17 +0200

    cors credential include

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import * as serviceWorker from "./serviceWorker";
 import { store } from "./Store";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URI + "/graphql" });
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URI + "/graphql", credentials: 'include' });
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem("token");
